0fef4fba0e1f07ac17507042a0fc2b77dff7f465
Dave Stevens <dave@dstvns.co.uk>
2015-12-27 11:22:34 +0000

    Includes Mocha running in Gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require("gulp"),
     gutil = require("gulp-util"),
     browserify = require("gulp-browserify"),
+    mocha = require("gulp-mocha"),
     Config = require("./config.json"),
     config = Config.development,
     log_error = (error) => {
@@ -17,11 +18,24 @@ gulp.task("scripts", () => {
     .pipe(gulp.dest(config.directory));
 });
 
+require('babel-core/register');
+gulp.task("test", () => {
+  return gulp
+    .src("test/**/*.spec.js", { read: false })
+    .pipe(
+      mocha({
+        reporter: "spec",
+        require: ["./test/helper"]
+      }).on("error", gutil.log)
+    );
+});
+
 gulp.task("development", () => { config = Config.development; });
 gulp.task("production", () => { config = Config.production; });
 
 gulp.task("watch", () => {
   gulp.watch(["scripts/**/*"], ["scripts"]);
+  gulp.watch(["scripts/**/*", "test/**/*"], ["test"]);
 });
 
 gulp.task("build", [
