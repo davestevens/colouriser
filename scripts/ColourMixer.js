69dcf59d6a4d144076be094155e4bc0da6d7ab6d
Dave Stevens <dave@dstvns.co.uk>
2015-12-27 12:34:56 +0000

    Implements ColourMixer.

---
+++ scripts/ColourMixer.js
@@ -0,0 +1,28 @@
+class ColourMixer {
+  constructor({ from, to, percentage }) {
+    this.from = from;
+    this.to = to;
+    this.percentage = percentage;
+  }
+
+  set percentage(value) {
+    this._percentage = Math.max(Math.min(value, 100), 0);
+  }
+  get percentage() { return this._percentage; }
+
+  set from(value) { this._from = value; }
+  get from() { return this._from; }
+
+  set to(value) { this._to = value; }
+  get to() { return this._to; }
+
+  getColour() {
+    let factor = this.percentage / 100;
+
+    return [ 0, 0, 0 ].map((_element, index) => {
+      return (this.to[index] - this.from[index]) * factor
+    });
+  }
+}
+
+module.exports = ColourMixer;
---
+++ test/ColourMixer.spec.js
@@ -0,0 +1,67 @@
+import ColourMixer from "../scripts/ColourMixer";
+
+describe("ColourMixer", () => {
+  describe("#percentage=", () => {
+    it("must be less than or equal to 100", () => {
+      let colourMixer = new ColourMixer({});
+      colourMixer.percentage = 250;
+
+      let actual = colourMixer.percentage,
+          expected = 100;
+
+      expect(actual).to.equal(expected);
+    });
+
+    it("must be greater than or equal to 0", () => {
+      let colourMixer = new ColourMixer({});
+      colourMixer.percentage = -250;
+
+      let actual = colourMixer.percentage,
+          expected = 0;
+
+      expect(actual).to.equal(expected);
+    });
+  });
+
+  describe("#getColour", () => {
+    let colourMixer;
+    before(() => {
+      let from = [   0,   0,   0 ],
+          to   = [   0,   0, 100 ];
+      colourMixer = new ColourMixer({ from: from, to: to });
+    });
+
+    context("with a percentage of zero", () => {
+      it("returns the 'from' colour", () => {
+        colourMixer.percentage = 0;
+
+        let actual = colourMixer.getColour(),
+            expected = colourMixer.from;
+
+        expect(actual).to.deep.equal(expected);
+      });
+    });
+
+    context("with a percentage of 100", () => {
+      it("returns the 'to' colour", () => {
+        colourMixer.percentage = 100;
+
+        let actual = colourMixer.getColour(),
+            expected = colourMixer.to;
+
+        expect(actual).to.deep.equal(expected);
+      });
+    });
+
+    context("with a percentage of 50", () => {
+      it("returns a mix of the two colours", () => {
+        colourMixer.percentage = 50;
+
+        let actual = colourMixer.getColour(),
+            expected = [   0,   0,  50 ];
+
+        expect(actual).to.deep.equal(expected);
+      });
+    });
+  });
+});
